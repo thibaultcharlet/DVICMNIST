87f2af3f4f1e8f0e38c0852d71a6c5856c547724
yliess86 <hatiyliess86@gmail.com>
2021-03-30 14:13:16 +0200

    Update for Training and Export

--- webmnist/__main__.py
+++ webmnist/__main__.py
@@ -1,10 +1,14 @@
 from argparse import ArgumentParser
+from webmnist.export import export
 from webmnist.train import train
 
 
 parser = ArgumentParser()
 parser.add_argument("-o", "--output", type=str, required=True)
 parser.add_argument("-e", "--epochs", type=int, default=3)
+parser.add_argument(      "--train",  action="store_true")
+parser.add_argument(      "--export", action="store_true")
 args = parser.parse_args()
 
-train(args.output, epochs=args.epochs)
+if args.train: train(args.output, epochs=args.epochs)
+if args.export: export(args.output)
